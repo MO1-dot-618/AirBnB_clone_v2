fc983c66e81042907a51c0f4068458100208d213
Malika <malikaoubilla@gmail.com>
2024-02-01 13:11:54 -0800

    1.1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,6 +5,7 @@ from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
     try:
@@ -16,4 +17,3 @@ def do_pack():
 
     except Exception as e:
         return None
-
