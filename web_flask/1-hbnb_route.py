8d2f67e854e255613d8789e3c1ce0571a4f517ca
Malika <malikaoubilla@gmail.com>
2024-02-18 20:35:54 +0000

    1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 def hello_hbnb():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
