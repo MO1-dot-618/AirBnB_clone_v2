98cee229cef0b13c7bfe07f57b3658d0ba5a6d8b
Malika <malikaoubilla@gmail.com>
2024-02-18 21:35:26 +0000

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,9 +24,9 @@ def display_c(text):
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
         <title>404 Not Found</title>
         <h1>Not Found</h1>
-        <p>The requested URL was not found on the server. 
-	If you entered the URL manually, please check your spelling
-	 and try again.</p>
+        <p>The requested URL was not found on the server.
+         If you entered the URL manually, please check your spelling
+         and try again.</p>
         """, 404
 
     # Replace underscores with spaces
